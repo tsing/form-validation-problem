d95db33f796084d0ff7f99cdbd232d6fc1d2b798
Jesse Wang <mr@jianqing.wang>
2018-06-11 11:46:09 +1200

    Implement form validation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,55 @@
+import validator from 'validator';
+
+const rules = {
+  email: val => {
+    return validator.isEmail(val);
+  },
+  username: val => {
+    return val.length >= 8;
+  },
+  colour: val => {
+    return val !== '';
+  },
+  animal: val => {
+    return val.length >= 2;
+  },
+  tiger_type: (val, formVal) => {
+    if (formVal.animal.includes('tiger')) {
+      return val !== '';
+    }
+    return true;
+  }
+};
+
 function onFormSubmit(event) {
   event.preventDefault();
-  alert('submitted');
+
+  const target = event.target;
+  const colourElement = target.querySelector('[name=colour]');
+
+  const formVal = {
+    email: target.querySelector('[name=email]').value,
+    username: target.querySelector('[name=username]').value,
+    colour: colourElement.options[colourElement.selectedIndex].value,
+    animal: Array.from(target.querySelectorAll('input[name=animal]'))
+      .filter(node => node.checked)
+      .map(node => node.value),
+    tiger_type: target.querySelector('[name=tiger_type]').value
+  };
+
+  let allGood = true;
+  for (const [name, func] of Object.entries(rules)) {
+    if (func(formVal[name], formVal) === false) {
+      target.querySelector('[name=' + name + ']').parentNode.className = 'error';
+      allGood = false;
+    } else {
+      target.querySelector('[name=' + name + ']').parentNode.className = '';
+    }
+  }
+
+  if (allGood) {
+    alert('successful');
+  }
 }
 
 const form = document.getElementsByTagName('form')[0];
